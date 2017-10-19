5ea51529de03e30f6d83b43398fe133b7af9813f
Unknown <philipp.comans@gmail.com>
2017-10-19 16:44:07 -0700

    implement Query#addSnapshotListener

--- android/src/main/kotlin/com/chq/firestore/FirestorePlugin.kt
+++ android/src/main/kotlin/com/chq/firestore/FirestorePlugin.kt
@@ -1,22 +1,27 @@
 package com.chq.firestore
 
-import com.google.firebase.firestore.CollectionReference
-import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.FirebaseFirestore
+import android.util.SparseArray
+import com.google.firebase.firestore.*
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import java.util.ArrayList
+import java.util.HashMap
 
-import com.google.firebase.firestore.Query
+class FirestorePlugin internal constructor(private val channel: MethodChannel) : MethodCallHandler {
+    private var nextHandle = 0
+    private val observers = SparseArray<EventObserver>()
+    private val documentObservers = SparseArray<DocumentObserver>()
+    private val listenerRegistrations = SparseArray<ListenerRegistration>()
 
-class FirestorePlugin() : MethodCallHandler {
+
     companion object {
         @JvmStatic
         fun registerWith(registrar: Registrar): Unit {
             val channel = MethodChannel(registrar.messenger(), "firestore")
-            channel.setMethodCallHandler(FirestorePlugin())
+            channel.setMethodCallHandler(FirestorePlugin(channel))
         }
     }
 
@@ -29,7 +34,59 @@ class FirestorePlugin() : MethodCallHandler {
                 documentReference.set(data)
                 result.success(null)
             }
+            "Query#addSnapshotListener" -> {
+                val arguments = call.arguments<Map<String, Any>>()
+                val handle = nextHandle++
+                val observer = EventObserver(handle)
+                observers.put(handle, observer)
+                listenerRegistrations.put(handle, getQuery(arguments).addSnapshotListener(observer))
+                result.success(handle)
+            }
             else -> result.notImplemented()
+        }
+    }
+
+    private inner class DocumentObserver internal constructor(private val handle: Int) : EventListener<DocumentSnapshot> {
+        override fun onEvent(documentSnapshot: DocumentSnapshot, e: FirebaseFirestoreException?) {
+            val arguments = HashMap<String, Any>()
+            arguments.put("handle", handle)
+            if (documentSnapshot.exists()) {
+                arguments["data"] = documentSnapshot.data;
+            }
+            channel.invokeMethod("DocumentSnapshot", arguments)
+        }
+    }
+
+
+    private inner class EventObserver internal constructor(private val handle: Int) : EventListener<QuerySnapshot> {
+
+        override fun onEvent(querySnapshot: QuerySnapshot, e: FirebaseFirestoreException?) {
+            val arguments = HashMap<String, Any>()
+            arguments.put("handle", handle)
+
+            val documents = querySnapshot.documents.map { it.data }
+            arguments.put("documents", documents)
+
+            val documentChanges = ArrayList<Map<String, Any>>()
+            for (documentChange in querySnapshot.documentChanges) {
+                val change = HashMap<String, Any>()
+                val type = when (documentChange.type) {
+                    DocumentChange.Type.ADDED -> "DocumentChangeType.added"
+                    DocumentChange.Type.MODIFIED -> "DocumentChangeType.modified"
+                    DocumentChange.Type.REMOVED -> "DocumentChangeType.removed"
+                }
+                change.put("type", type)
+                change.put("oldIndex", documentChange.oldIndex)
+                change.put("newIndex", documentChange.newIndex)
+                //        DocumentSnapshot doc = documentChange.getDocument();
+                //        Map<String, Object> docData = doc.getData();
+                //        docData.put("key", doc.getId());
+                change.put("document", documentChange.document.data)
+                documentChanges.add(change)
+            }
+            arguments.put("documentChanges", documentChanges)
+
+            channel.invokeMethod("QuerySnapshot", arguments)
         }
     }
 
@@ -45,3 +102,5 @@ class FirestorePlugin() : MethodCallHandler {
         return FirebaseFirestore.getInstance().document(path)
     }
 }
+
+
