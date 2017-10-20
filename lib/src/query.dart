b5da40a87d1fd3f07d3127472e80fdfd244cb705
Andy Zhang <andy@carnivorestudios.com>
2017-10-19 17:41:00 -0700

    Clean up package names and MyApp name

--- lib/src/query.dart
+++ lib/src/query.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-part of firebase_firestore;
+part of firestore;
 
 /// Represents a query over the data at a particular location.
 class Query {
